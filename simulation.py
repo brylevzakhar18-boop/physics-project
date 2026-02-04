e7df08f0d09cae1996bf3d72dc14f71f9fbe5f46
brylevzakhar18-boop <brylev.zakhar18@gmail.com>
2026-02-04 19:51:18 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,1 +1,12 @@
-simulation.py
+import math
+
+speed = 20
+angle = 45
+
+angle_rad = math.radians(angle)
+
+vx = speed * math.cos(angle_rad)
+vy = speed * math.sin(angle_rad)
+
+print("Horizontal speed:", round(vx, 2))
+print("Vertical speed:", round(vy, 2))
